f23b36b66914ed1cc362f9174f5b7f96459a060c
olinares <olinares9833@gmail.com>
2014-12-07 12:59:56 -0800

    Add views, add destroy feature

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,12 +4,46 @@ class EventsController < ApplicationController
     @event = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to(event_path())
+    else
+      render :edit
+    end
+  end
+
   def new
     @event = Event.new
   end
 
-  def show
+  def edit
+    @event = Event.find(params[:id])
   end
 
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      redirect_to events_path
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :description, :require_id, :capacity, :directions)
+  end
 end
